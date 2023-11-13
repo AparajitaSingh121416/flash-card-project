3eb049261f1bc7d047e4caf74ad940822561dd96
Aparajita Singh <aparajitalnctscse@gmail.com>
2023-11-13 20:13:15 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+BACKGROUND_COLOR = "#B1DDC6"
+# <------------------------------- UI SETUP ----------------------------------------------->
+from tkinter import *
+window = Tk()
+window.config(bg=BACKGROUND_COLOR, width=700, height=500)
+window.title("Flashy")
+window.mainloop()
